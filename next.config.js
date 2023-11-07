85c0761b3a60041a7ee2326e2d7bafea0ff288e7
shaynlink <guemmoudrayane03@gmail.com>
2023-11-07 10:50:12 +0100

    fix next.config.js#basePath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/gh',
+    basePath: '/gh-pages',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     }
