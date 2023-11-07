7b81f44b25ef748be67e1769a91f7216c3047800
shaynlink <guemmoudrayane03@gmail.com>
2023-11-07 10:49:18 +0100

    add deploy to github page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const path = require('node:path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    basePath: '/gh',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     }
